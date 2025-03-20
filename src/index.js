6e5c2346e43a7fedc54d3969c8a64db3f34091bf
driveGosling <asemlanva@gmail.com>
2025-03-20 12:25:39 +0300

    Updates

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import QuestionCard from "./components/QuestionCard.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<QuestionCard />);
