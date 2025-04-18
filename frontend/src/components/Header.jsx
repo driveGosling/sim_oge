1a874e0c5c5681b81b8267139f89c60ebeb1c925
KudesnikRaph <kolesnik.akk@gmail.com>
2025-04-18 16:28:35 +0300

    Add new header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,28 @@
-import './Header.css'
-import { Link } from "react-router-dom";
+import './Header.css';
+import { Link } from 'react-router-dom';
+import logoSrc from '../assets/logo.png';
+import peterSrc from '../assets/peter.png';
+import bookSrc from '../assets/book.png';
 
 const Header = () => {
   return (
     <header className="header">
-      <Link to="/"><img src="./src/assets/logo.png" alt="logo" className="logo" /></Link>
-      <h1 className="title">Сдам ГИА: РЕШУ ОГЭ</h1>
+      <div className="logo-container">
+        <Link to="/">
+          <img src={logoSrc} alt="logo" className="logo" />
+        </Link>
+        <h2 className="subtitle">История россии</h2>
+      </div>
+      <div
+        className="photo-container"
+      >
+        <img src={bookSrc} alt="Книги" className="books" />
+        <img
+          src={peterSrc}
+          alt="Пётр Первый"
+          className="photo"
+        />
+      </div>
     </header>
   );
 };
