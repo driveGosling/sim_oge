668f8f36988e00ccd0e3dee04da2333a26b852c6
KudesnikRaph <kolesnik.akk@gmail.com>
2025-04-18 16:31:07 +0300

    Upd MainPage Ok?

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,33 +1,35 @@
-import "./MainPage.css";
-import { Link } from "react-router-dom";
-import VariantConstructor from "../components/VariantConstructor"
+import React from 'react';
+import './MainPage.css';
+import { Link } from 'react-router-dom';
+import VariantConstructor from '../components/VariantConstructor';
 
-const Main = ({ variantsList }) => {
-  return (
-    <main className="main">
-      <section className="section">
-        <h2>Тренировочные варианты</h2>
-        <ul>
-          {variantsList.map((v) => (
-            <li key={v.id}>
-              <Link to={`/test/${v.id}`}>{v.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </section>
-      <section className="section">
-        <h2>Конструктор варианта по темам</h2>
-        <p className="descr">
-          Что­бы це­ле­на­прав­лен­но тре­ни­ро­вать­ся по опре­де­лён­ным
-          те­мам, вы мо­же­те со­ста­вить ва­ри­ант из необ­хо­ди­мо­го
-          ко­ли­че­ства за­да­ний по кон­крет­ным раз­де­лам за­дач­но­го
-          ка­та­ло­га. Для быст­ро­го со­став­ле­ния ти­по­во­го ва­ри­ан­та
-          ис­поль­зуй­те кноп­ки спра­ва.
-        </p>
-        <VariantConstructor />
-      </section>
-    </main>
-  );
-};
+// Логику не понял, исправить если надо, то исправим
+const Main = ({ variantsList }) => (
+  <main className="main">
+    <section className="section-tren">
+      <h2>Тренировочные варианты</h2>
+      <ul className="variants-list">
+        <span className="corner-bl" />
+        <span className="corner-br" />
+        {Array.from({ length: 20 }, (_, idx) => (
+          <li className="li-test" key={idx + 1}>
+            <Link to={`/test/${idx + 1}`}>{`Вариант ${idx + 1}`}</Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+
+      <h2>Конструктор вариантов</h2>
+    <div className="descr-wrapper">
+      <span className="corner-bl" />
+      <span className="corner-br" />
+      <p className="descr">
+        Чтобы целенаправленно тренироваться по определённым темам, вы можете составить вариант из
+        необходимого количества заданий по конкретным разделам каталога.
+      </p>
+    </div>
+    <VariantConstructor />
+  </main>
+);
 
 export default Main;
