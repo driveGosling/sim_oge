41c9221dd323f76c86ae6f9b250d11401b3dcb81
driveGosling <asemlanva@gmail.com>
2025-04-20 14:35:10 +0300

    update Question component to handle new short/long answer logic

--- frontend/src/components/Question.jsx
+++ frontend/src/components/Question.jsx
@@ -1,4 +1,4 @@
-import './Question.css'
+import "./Question.css";
 
 const Question = ({ question, isSubmitted, userAnswer, onAnswerChange }) => {
   const isCorrect = isSubmitted && userAnswer === question.correctAnswer;
@@ -6,17 +6,41 @@ const Question = ({ question, isSubmitted, userAnswer, onAnswerChange }) => {
   return (
     <div className="question-card">
       <h3>{question.text}</h3>
+
       <label>
-        <input
-          type="text"
-          name={question.id}
-          disabled={isSubmitted}
-          value={userAnswer}
-          onChange={(e) => onAnswerChange(question.id, e.target.value)}
-          className={isSubmitted ? (isCorrect ? 'correct' :  'wrong') : ''}
-          />
+        {question.answerType === "short" ? (
+          question.answerOptions.map((option) => (
+            <div key={option.id}>
+              <input
+                type="radio"
+                name={question.id}
+                value={option.optionText}
+                checked={userAnswer === option.optionText}
+                disabled={isSubmitted}
+                onChange={(e) => onAnswerChange(question.id, e.target.value)}
+              />
+              {option.optionText}
+            </div>
+          ))
+        ) : (
+          <>
+            <p>
+              Решения заданий с развернутым ответом не проверяются
+              автоматически.
+            </p>
+            <input
+              type="text"
+              name={question.id}
+              disabled={isSubmitted}
+              value={userAnswer}
+              onChange={(e) => onAnswerChange(question.id, e.target.value)}
+              className={isSubmitted ? (isCorrect ? "correct" : "wrong") : ""}
+            />
+          </>
+        )}
       </label>
-      {isSubmitted ? (!isCorrect ? (<div>{question.correctAnswer}</div>) : '') : ''}
+
+      {isSubmitted ? !isCorrect ? <div>{question.correctAnswer}</div> : "" : ""}
     </div>
   );
 };
