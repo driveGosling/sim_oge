83305a71cd8a06028ee8f712d7f3ae5b2793a1bc
driveGosling <asemlanva@gmail.com>
2025-05-05 17:51:44 +0300

    restructure backend directory

---
+++ backend/src/routes/apiRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const apiController = require("../controllers/apiController");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../..", "public/uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueName = Date.now() + "-" + file.originalname;
+    cb(null, uniqueName);
+  },
+});
+const upload = multer({ storage: storage });
+
+router.get("/variants", apiController.getVariants);
+router.get("/topics", apiController.getTopics);
+router.get("/questions", apiController.getQuestions);
+router.post("/questions", upload.single("image"), apiController.createQuestion);
+router.post("/variants", apiController.createVariant);
+
+module.exports = router;
