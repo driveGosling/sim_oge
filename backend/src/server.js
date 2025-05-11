a6cb55d15306b3e7659540c2e709b04ad070759a
KudesnikRaph <kolesnik.akk@gmail.com>
2025-05-11 20:51:19 +0300

    backend check

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,13 @@ const apiRouter = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const pool = new Pool({
+  host:     process.env.DATABASE_HOST,
+  port:     process.env.DATABASE_PORT,
+  user:     process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
 
 app.use(cors());
 app.use(express.json());
@@ -14,6 +21,17 @@ console.log(">> Using DATABASE_URL:", process.env.DATABASE_URL);
 app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 app.use("/images", express.static(path.join(__dirname, "../public/uploads")));
 
+app.get("/api/health", async (_, res) => {
+  try {
+    const { rows } = await pool.query("SELECT NOW()");
+    res.json({ dbTime: rows[0].now });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "DB connection failed" });
+  }
+});
+
+
 app.use("/api", apiRouter);
 
 app.get("*", (req, res) => {
@@ -21,5 +39,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http:${port}`);
 });
